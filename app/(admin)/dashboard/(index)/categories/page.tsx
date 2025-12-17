a64a2153210f21dad6b2d8640cb4b7ceb6207262
Mahen <mahennekkers27@gmail.com>
2025-12-17 08:15:21 +0700

    chore: add categories data table.

---
+++ app/(admin)/dashboard/(index)/categories/page.tsx
@@ -0,0 +1,11 @@
+import { DataTable } from "@/components/ui/data-table";
+import { getCategories } from "./lib/data";
+
+export default async function CategoriesPage() {
+  const data = await getCategories();
+  return (
+    <>
+      <DataTable data={data} />
+    </>
+  );
+}
