712dc146a580c6a345ecd7d39104d50a74dffda7
Mahen <mahennekkers27@gmail.com>
2026-01-07 11:12:40 +0700

    fix: comment useless temporary file

--- app/(admin)/dashboard/(index)/orders/create/page.tsx
+++ app/(admin)/dashboard/(index)/orders/create/page.tsx
@@ -1,13 +1,13 @@
-import FormCategory from "../_components/form-category";
+// import FormCategory from "../_components/form-category";
 
-export default async function CreatePage() {
-  return (
-    <div className="max-w-2xl mx-auto py-10">
-      <div className="mb-6">
-        <h1 className="text-2xl font-bold">Add Category</h1>
-        <p className="text-muted-foreground">Make your category here.</p>
-      </div>
-      <FormCategory />
-    </div>
-  );
-}
+// export default async function CreatePage() {
+//   return (
+//     <div className="max-w-2xl mx-auto py-10">
+//       <div className="mb-6">
+//         <h1 className="text-2xl font-bold">Add Category</h1>
+//         <p className="text-muted-foreground">Make your category here.</p>
+//       </div>
+//       <FormCategory />
+//     </div>
+//   );
+// }
--- app/(admin)/dashboard/(index)/orders/edit/categories-form-wrapper.tsx
+++ app/(admin)/dashboard/(index)/orders/edit/categories-form-wrapper.tsx
@@ -1,16 +1,16 @@
-import { notFound } from "next/navigation";
-import { getCategoryById } from "../lib/actions";
-import FormUpdateCategory from "../_components/form-update-category";
-import { PageProps } from "@/lib/types";
+// import { notFound } from "next/navigation";
+// import { getCategoryById } from "../lib/actions";
+// import FormUpdateCategory from "../_components/form-update-category";
+// import { PageProps } from "@/lib/types";
 
-export default async function CategoriesFormWrapper({ params }: PageProps) {
-  const { id } = await params;
+// export default async function CategoriesFormWrapper({ params }: PageProps) {
+//   const { id } = await params;
 
-  const data = await getCategoryById(Number(id));
+//   const data = await getCategoryById(Number(id));
 
-  if (!data) {
-    return notFound();
-  }
+//   if (!data) {
+//     return notFound();
+//   }
 
-  return <FormUpdateCategory data={data} />;
-}
+//   return <FormUpdateCategory data={data} />;
+// }
